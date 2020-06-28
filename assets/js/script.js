a43935e8a3dbf499adb92cecd1fb44d37528be67
unknown <pv95335@gmail.com>
2020-06-28 16:02:47 -0700

    Recieve data from open weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,58 @@
-var weatherInfo = function(){
-    fetch("api.openweathermap.org/data/2.5/forecast?id="+ theCity +"&appid=00d04ffea22670d2825eac31941d3a6a")
-    console.log("function is called");
+var weatherContainerEl = document.querySelector("#weather-container");
+var weatherSearchTerm = document.querySelector("#weather-search-term");
+
+var getCityInfo= function(cityResponse) {
+
+var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityResponse + "&units=imperial&appid=00d04ffea22670d2825eac31941d3a6a";
+
+fetch(apiURL)
+.then(function(response) {
+    if (response.ok) {
+        response.json().then(function(data) {
+            displayCity(data,cityResponse);
+        });
+    } else {
+        alert("Error: " + response.statusText);
+    }
+})
+.catch(function(error) {
+    alert("Unable to connect to OpenWeatherMaps");
+})
+console.log(apiURL)
+};
+
+var cityFormEl = document.querySelector("#searchCity");
+
+var cityInputEl = document.querySelector("#cityName");
+
+var formSubmitHandler = function() {
+    event.preventDefault();
+    //var theCity = cityInputEl.nodeValue.trim();
+
+    if (cityInputEl.value) {
+        getCityInfo(cityInputEl.value);
+        nameInputEl.value= "";
+    } else {
+        alert("Please enter a city name");
+    }
+    console.log(event)
 }
 
-var theCity = function(){
-    localStorage.setItem(theCity);
-    document.getElementById("#theCity").innerHTML = localStorage.getItem(theCity);
+var displayCity = function(city, searchTerm) {
+    if(city.length ===0) {
+        cityContainerEl.textContent = "No cities found.";
+        return;
+    }
+
+weatherContainerEl.textContent = "";
+weatherSearchTerm.textContent = searchTerm;
+//console.log(weather);
+console.log(searchTerm);
+
+//$("weather-container").appendChild("cityName")
+
 }
 
+//weatherContainerEl.addEventListener("cityName",formSubmitHandler);
 
-weatherInfo();
+getCityInfo("Chicago");
